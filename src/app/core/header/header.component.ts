17d4c459abc4fd974d72ac91fbd961772357ab18
Tonaxe <ymennana@elpuig.xeill.net>
2025-10-15 18:06:27 +0200

    5 commit

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, ElementRef, HostListener, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../pages/auth/auth.service';
+import { AuthStore } from '../../pages/auth/auth.store';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  private store = inject(AuthStore);
+  private auth  = inject(AuthService);
+  private router = inject(Router);
+  private el = inject(ElementRef);
+
+  auth$ = this.store.auth$;
+
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  async logout() {
+    this.menuOpen = false;
+    this.auth.logout().subscribe({
+      next: () => this.router.navigateByUrl('/login'),
+      error: () => { this.auth.clearLocal(); this.router.navigateByUrl('/login'); }
+    });
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocClick(e: MouseEvent) {
+    if (!this.el.nativeElement.contains(e.target)) this.menuOpen = false;
+  }
+}
